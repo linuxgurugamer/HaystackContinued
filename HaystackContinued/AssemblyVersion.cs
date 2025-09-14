9af700499cff85b94d725ad9fd1aa4f85ba9b6ac
linuxgurugamer@gmail.com <linuxgurugamer@gmail.com>
2025-09-14 08:53:06 -0400

    Updated version file to use github for new version info

--- HaystackContinued/AssemblyVersion.cs
+++ HaystackContinued/AssemblyVersion.cs
@@ -5,5 +5,5 @@
   
  using System.Reflection;
 
- [assembly: AssemblyVersion("0.5.7.5")]
- [assembly: AssemblyFileVersion("0.5.7.5")]
+ [assembly: AssemblyVersion("0.5.7.6")]
+ [assembly: AssemblyFileVersion("0.5.7.6")]
